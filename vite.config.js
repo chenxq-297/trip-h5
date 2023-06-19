ad885436d7780c0fbad8c0c2d6c54ffb8f97d8c1
cxq <1037216783@qq.com>
2023-06-19 19:27:10 +0800

    配置postcssToVw

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
 
+import postcssPxToViewport from 'postcss-px-to-viewport'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -16,6 +18,17 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css:{
+    postcss:{
+      plugins:[
+        postcssPxToViewport({
+          viewportWidth: 375,
+          // rootValue: 16, // 1rem的大小
+          propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
+        })
+      ]
     }
   }
 })
